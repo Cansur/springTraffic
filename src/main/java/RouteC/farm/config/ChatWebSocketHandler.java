118bb5a7d795fb9a5b86529b0b351d9386a4cbef
Cansur <tjswo0682@gmail.com>
2024-05-21 18:37:23 +0900

    WebSocket 추가 구성

--- src/main/java/RouteC/farm/config/ChatWebSocketHandler.java
+++ src/main/java/RouteC/farm/config/ChatWebSocketHandler.java
@@ -13,17 +13,19 @@ import org.springframework.web.socket.handler.TextWebSocketHandler;
 @SuppressWarnings("null")
 public class ChatWebSocketHandler extends TextWebSocketHandler{
 
+    // 세션 여러개 분할 필요.
+
     // CLIENTS 변수에 session 담기
     private static final ConcurrentHashMap<String, WebSocketSession> CLIENTS = new ConcurrentHashMap<String, WebSocketSession>();
 
-    // 연결이 되면 함수 호출
+    // 연결 확인
     @Override
     public void afterConnectionEstablished(WebSocketSession session) throws Exception {
         System.out.println("id :" + session.getId() + " 님이 연결되었습니다.");
         CLIENTS.put(session.getId(), session);
     }
 
-    // 연결이 종료되면 함수 호출
+    // 연결 종료
     @Override
     public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
         System.out.println("id :" + session.getId() + " 님이 연결 종료되었습니다.");
@@ -31,6 +33,9 @@ public class ChatWebSocketHandler extends TextWebSocketHandler{
     }
 
     // 메시지가 보내지면 함수 호출
+    // ChatMessageDto 양식을 사용하여 송신 및 수신
+    // 메모리 상 채팅방 세션이 없을 경우 생성 시스템 필요
+    // restapi를 사용하여 api 구성 제작 필요
     @Override
     protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
         String id = session.getId();  //메시지를 보낸 아이디
